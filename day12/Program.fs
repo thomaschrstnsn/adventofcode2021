20c9f4b1fe980ace8d47ab2beb64157eade10e90
Thomas Christensen <thomas@chrstnsn.dk>
2021-12-13 08:22:01 +0100

    just started day 12

---
+++ day12/Program.fs
@@ -0,0 +1,11 @@
+﻿
+type Node = string
+
+type Graph = Map<Node,Node>
+
+let parseLine (line:string) = 
+    match line.Split '-' with
+    | [| ns ; ne |] -> (ns,ne)
+    | _ -> failwith (sprintf "failed to parse '%s'" line)
+
+let parse (inps : string array) : Graph =  inps |> Array.map parseLine |> Map.ofArray
